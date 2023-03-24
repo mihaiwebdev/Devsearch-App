5e6b618ee3243f0fa543b959cb74699cc2c2876d
Mihai Sava <1mihaisava1@gmail.com>
2023-03-24 10:16:29 +0200

    Update signals.py

--- users/signals.py
+++ users/signals.py
@@ -1,7 +1,6 @@
 from django.db.models.signals import post_save, post_delete
 from .models import Profile
 from django.contrib.auth.models import User
-from django.core.mail import send_mail
 from django.conf import settings
 
 
@@ -14,17 +13,7 @@ def createProfile(sender, instance, created, **kwargs):
             email=user.email,
             name=user.first_name,
         )
-
-        subject = 'Welcome to DevSearch'
-        message = 'We are glad you are here!'
-
-        send_mail(
-            subject,
-            message,
-            settings.EMAIL_HOST_USER,
-            [profile.email]
-        )
-
+        
         profile.save()
 
 
